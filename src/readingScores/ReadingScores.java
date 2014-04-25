07bd9098641b5c9f3b5be894caea3d2d963cc4c2
danielpe@rams.colostate.edu <danielpe@rams.colostate.edu>
2014-04-24 20:55:08 -0600

    Seperating my concerns, separating functionality into classes

--- src/readingScores/ReadingScores.java
+++ src/readingScores/ReadingScores.java
@@ -1,7 +1,6 @@
 package readingScores;
 
 import java.io.IOException;
-import java.util.StringTokenizer;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
@@ -11,29 +10,7 @@ import org.apache.hadoop.mapreduce.lib.input.*;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 public class ReadingScores {
-	public class ScoreMapper extends Mapper <LongWritable ,Text , Text , IntWritable> {
-		public void map(LongWritable key , Text value , Context context) throws IOException , InterruptedException {
-			String line = value.toString();
-			StringTokenizer tok = new StringTokenizer(line);
-			while(tok.hasMoreTokens()) {
-				context.write(new Text(tok.nextToken()) , new IntWritable(1));
-			}
-		} //end map
-	} //end mapper class
-
-
-	public class ScoreReducer extends Reducer< Text , IntWritable , Text , IntWritable > {
-		public void reduce(Text key, Iterable <IntWritable> values, Context context ) throws IOException , InterruptedException {
-			int sum = 0;
-			for( IntWritable i : values ) {
-				sum += i.get();
-			}
-			context.write( key , new IntWritable( sum ));
-		} //end reduce
-	}//end reducer class
-
-	public static void main( String [] args ) throws IOException ,
-	ClassNotFoundException,InterruptedException {
+	public static void main( String [] args ) throws IOException, ClassNotFoundException, InterruptedException {
 		Job job = Job.getInstance (new Configuration ());
 		job.setJarByClass(ReadingScores.class); //this class’s name
 		job.setJobName("Word Count"); //name of this job.
